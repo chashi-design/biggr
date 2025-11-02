d5c333c57743684533e5a488450460b7101018f3
thirohas <chashi.design@gmail.com>
2025-11-02 21:03:47 +0900

    Initial Commit

---
+++ TrainLogApp/TrainLogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainLogAppApp.swift
+//  TrainLogApp
+//
+//  Created by Takanori Hirohashi on 2025/11/02.
+//
+
+import SwiftUI
+
+@main
+struct TrainLogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
